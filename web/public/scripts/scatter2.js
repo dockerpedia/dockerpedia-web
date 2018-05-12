95bb573b3f09f3c46264c8cdca530d6d842670a6
Hernán Vargas <hernan.vargas@alumnos.usm.cl>
2018-05-12 00:12:23 -0300

    tooltip and radius fix

--- web/public/scripts/scatter2.js
+++ web/public/scripts/scatter2.js
@@ -120,7 +120,7 @@ var start = function(data) {
       .data(data)
     .enter().append("circle")
       .attr("class", "dot")
-      .attr("r", 3.5)
+      .attr("r", 5.5)
       .attr("cx", xMap)
       .attr("cy", yMap)
       .style("fill", function(d) { return color(cValue(d));}) 
@@ -129,8 +129,7 @@ var start = function(data) {
           tooltip.transition()
                .duration(200)
                .style("opacity", .9);
-          tooltip.html(d["Cereal Name"] + "<br/> (" + xValue(d) 
-	        + ", " + yValue(d) + ")")
+          tooltip.html(d["Cereal Name"]) //+ "<br/> (" + xValue(d) + ", " + yValue(d) + ")")
                .style("left", (d3.event.pageX + 5) + "px")
                .style("top", (d3.event.pageY - 28) + "px");
       })
