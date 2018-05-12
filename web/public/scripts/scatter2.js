441ccb02784644e6ea5d47b37cfdf7c902d37a35
Hernán Vargas <hernan.vargas@alumnos.usm.cl>
2018-05-12 01:58:13 -0300

    side to side, dinamic width

--- web/public/scripts/scatter2.js
+++ web/public/scripts/scatter2.js
@@ -35,9 +35,10 @@ angular.module('dockerpedia.directives')
 
       /** MAIN SVG **/
 
+var parentWidth = element[0].parentElement.offsetWidth;
 
 var margin = {top: 20, right: 20, bottom: 30, left: 80},
-    width = 960 - margin.left - margin.right,
+    width = parentWidth - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 // setup x 
